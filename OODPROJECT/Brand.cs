9560f95d54d1c5ec851f0ebeeeac0c02d3a2f7b1
Clara Monteiro <S00185743@mail.itsligo.ie>
2020-01-29 15:31:32 +0000

    Add project files.

---
+++ OODPROJECT/Brand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODPROJECT
+{
+
+    class Brand
+    {
+        public string Name { get; set; }
+
+        public string Description;
+
+        public Brand (string name)
+        {
+            Name = name;
+        }
+
+        public Brand()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ";
+        }
+    }
+}
---
+++ OODPROJECT/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OODPROJECT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Brand> AllBrands = new List<Brand>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // create shoe brand 
+
+            Brand b1 = new Brand()
+            {
+                Name = "Air force 1",
+                Description = "1"
+            };
+            Brand b2 = new Brand()
+            {
+                Name = "Nike Jordan",
+                Description = "1"
+            };
+
+           
+
+            
+        }
+    }
+}
